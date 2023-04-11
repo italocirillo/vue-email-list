1fcb7226402ddb3c8bf8cc55a1fd3d7d8380468e
italocirillo <italocirillo@gmail.com>
2023-04-11 19:19:39 +0200

    ESERCIZIO FATTO

--- js/script.js
+++ js/script.js
@@ -1,1 +1,20 @@
-console.log("js collegato");
+const { createApp } = Vue;
+
+createApp({
+    data(){
+        return{
+            listaEmail:[null],
+            numeroIndirizzi : 10,
+        };
+    },
+    mounted(){
+        for(let i=0;i<this.numeroIndirizzi;i++){
+            axios
+            .get("https://flynn.boolean.careers/exercises/api/random/mail")
+            .then((risposta) => {
+                this.listaEmail[i] = risposta.data.response;
+                console.log(i,this.listaEmail[i]);
+            })
+        }
+    }
+}).mount("#app");
