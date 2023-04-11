f3bd03977322288b1e9f4542c67f7c60c63a864a
italocirillo <italocirillo@gmail.com>
2023-04-11 20:57:55 +0200

    LOGICA COMPLETA

--- js/script.js
+++ js/script.js
@@ -8,13 +8,18 @@ createApp({
         };
     },
     mounted(){
-        for(let i=0;i<this.numeroIndirizzi;i++){
-            axios
-            .get("https://flynn.boolean.careers/exercises/api/random/mail")
-            .then((risposta) => {
-                this.listaEmail[i] = risposta.data.response;
-                console.log(i,this.listaEmail[i]);
-            })
+        this.generaLista(this.listaEmail);
+    },
+    methods: {
+        generaLista(array){
+            for(let i=0;i<this.numeroIndirizzi;i++){
+                axios
+                .get("https://flynn.boolean.careers/exercises/api/random/mail")
+                .then((risposta) => {
+                    array[i] = risposta.data.response;
+                    console.log(i,array[i]);
+                })
+            }
         }
     }
 }).mount("#app");
